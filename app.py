276c154113b7d2ee3174c4991acc8591d4cf6e24
edomega24-pixel <edomega24@gmail.com>
2026-09-01 20:19:15 -0500

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from google import genai
 from datetime import date
 
-# Cliente moderno con tu API key
 client = genai.Client(api_key="AQ.Ab8RN6KujP4ma5SacCmWm009IJF32R7e3oKpXK3fhGZjxn6XZA")
 
 st.set_page_config(page_title="Apuestas Claras y Rápidas", layout="centered")
